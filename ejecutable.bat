8a6785c060f74257bd286f737dea52300225e5f0
Ivan <ivangauto602160@gmail.com>
2025-06-20 20:39:59 -0300

    Arreglo de ejecutable, prueba de ejecucion en otra maquina

--- ejecutable.bat
+++ ejecutable.bat
@@ -4,14 +4,14 @@ setlocal
 REM Cambiar la consola a UTF-8 para soportar ñ y caracteres especiales
 chcp 65001 > nul
 
-REM Ruta al JDK (ajustá si está en otro lado)
-set "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.5.11-hotspot"
-set "PATH=%JAVA_HOME%\bin;%PATH%"
+REM Obtener la ruta donde está el .bat
+set "SCRIPT_DIR=%~dp0"
 
-REM Ruta al SDK de JavaFX
-set "JAVA_FX_LIB=javafx-sdk-21.0.7\lib"
+REM Ruta relativa a la carpeta lib de JavaFX (debe estar junto al .bat)
+set "JAVA_FX_LIB=%SCRIPT_DIR%javafx-sdk-21.0.7\lib"
 
-REM Ejecutar
-java --module-path "%JAVA_FX_LIB%" --add-modules javafx.controls,javafx.fxml -jar SignalAnalyzer.jar
+REM Ejecutar usando java que esté en el PATH del sistema
+java --module-path "%JAVA_FX_LIB%" --add-modules javafx.controls,javafx.fxml -jar "%SCRIPT_DIR%SignalAnalyzer.jar"
 
 pause
+
